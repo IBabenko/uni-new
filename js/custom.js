30af68ea0d9328e96b5502a31a87f465bdbcfa73
Igor Babenko <devel.web@outlook.com>
2019-10-09 09:18:42 +0300

    index нужен для переадресации на index1 и mobile. Верстка перенесена на bs4. Довавлена пункт меню - презентация

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,34 @@
-﻿$(window).on('load', function () {
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-		$('body').addClass('ios');
-	} else{
-		$('body').addClass('web');
-	};
-	$('body').removeClass('loaded'); 
+﻿
+
+$('#form').on('submit', function(e){
+	e.preventDefault();
+	let fd = new FormData( this );
+	$.ajax({
+		url: 'ContactMailer.php',
+		type: 'POST',
+		contentType: false,
+		processData: false,
+		data: fd,
+		success: function(msg){
+			if(msg == 1) {
+				alert('Отправлено');
+			} else {
+				alert('Ошибка')
+			}
+		}
+	});
+});
+
+
+
+$(window).on('load', function () {
+
+	$('body').removeClass('loaded');
 
 	$('.about').addClass('anim');
-	
-	DetectTheThing()
 
 });
 
-function DetectTheThing() {
-    	if( screen.width <= 480 && screen.height <= 812 ) {
-    		location.href = '../Universum canvas new/mobil.html';
-  		} else if( screen.width <= 812 && screen.height <= 480 ) {
-    		location.href = '../Universum canvas new/mobil.html';
-  			}
-		// } else {
-		// 	location.href = 'index.html';
-		// }
-       // var uagent = navigator.userAgent.toLowerCase();
-       // if (uagent.search("iphone") > -1 || uagent.search("ipad") > -1 
-       // || uagent.search("android") > -1 || uagent.search("blackberry") > -1
-       // || uagent.search("webos") > -1)
-       //    window.location.href ="mobil.html";
-    }
 // var screen = $(window).width();
 // if( screen <= 480 ) {
 //     location.href = '/mobile.html';
